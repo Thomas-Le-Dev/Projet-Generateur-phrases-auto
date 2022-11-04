7b976561cc83c8a1a434fa63d507f1be0154172c
Thomas-Le-Dev <82824104+Thomas-Le-Dev@users.noreply.github.com>
2022-11-04 14:45:28 +0100

    Add files via upload

---
+++ fonctions.h
@@ -0,0 +1,22 @@
+#ifndef TP1_FUNCTION_H
+#define TP1_FUNCTION_H
+#endif //TP1_FUNCTION_H
+
+#define ALPHABET_SIZE 26
+
+
+
+//FONCTION
+
+
+p_tree MakeTree();
+
+void readTree(t_tree);
+
+void AddNode(p_tree);
+
+void RmNode(p_tree);
+
+void create_sentences(t_tree);
+
+mots chercher_mots(t_tree);
